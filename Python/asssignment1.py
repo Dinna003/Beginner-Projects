87b640045c331f9389d696459508dca3033c7bf9
Dinna003 <dinethwickremasinghe3@gmail.com>
2025-01-08 19:13:44 +0530

    random_moratu_quiz

---
+++ Python/asssignment1.py
@@ -0,0 +1,19 @@
+import requests
+
+url = 'http://host1.open.uom.lk:8080/'
+
+updated_entity = {
+        "productName":"Araliya Basmathi Rice",
+        "description":"White Basmathi Rice imported from Pakistan. High-quality rice with extra fragrance. Organically grown.",
+        "category":"Rice",
+        "brand":"Araliya",
+        "expiredDate":"2023.05.04",
+        "manufacturedDate":"2022.02.20",
+        "batchNumber":324567,
+        "unitPrice":1020,
+        "quantity":200,
+        "createdDate":"2022.02.24"
+}
+
+response = requests.put(f"{url}/api/products/")
+print(response.json())
